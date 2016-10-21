525d91318049580bf7e176cfddaee37cf2a4b9e7
Sergey Tereschenko <serg.partizan@gmail.com>
2016-10-21 14:03:45 +0300

    smallfixes

--- django_beanstalkd/__init__.py
+++ django_beanstalkd/__init__.py
@@ -7,17 +7,20 @@ from beanstalkc import Connection, SocketError, DEFAULT_PRIORITY, DEFAULT_TTR
 
 from decorators import beanstalk_job
 
+__all__ = ['beanstalk_job', 'connect_beanstalkd']
+
 def connect_beanstalkd():
     """Connect to beanstalkd server(s) from settings file"""
 
     server = getattr(settings, 'BEANSTALK_SERVER', '127.0.0.1')
+    timeout = getattr(settings, 'BEANSTALK_CONNECT_TIMEOUT', 10)
     port = 11300
     if server.find(':') > -1:
         server, port = server.split(':', 1)
-        
+
     try:
         port = int(port)
-        return Connection(server, port)
+        return Connection(server, port, connect_timeout=timeout)
     except (ValueError, SocketError), e:
         raise BeanstalkError(e)
 
@@ -36,7 +39,7 @@ class BeanstalkClient(object):
         """
         Calls the specified function (in beanstalk terms: put the specified arg
         in tube func)
-        
+
         priority: an integer number that specifies the priority. Jobs with a
                   smaller priority get executed first
         delay: how many seconds to wait before the job can be reserved
