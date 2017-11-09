b2f3a15f1237af4ae6865324bcdd18e2e379e431
Sergey Tereschenko <serg.partizan@gmail.com>
2017-11-09 18:02:03 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-beanstalkd',
-    version='0.2.1',
+    version='0.2.2',
     description='A convenience wrapper for beanstalkd clients and workers '
                 'in Django using the beanstalkc library for Python',
     long_description=open('README.md').read(),
